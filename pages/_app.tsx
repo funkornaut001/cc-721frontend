948a86ac9aa3e5d7a67f23d21ab86537edbfc62b
funkornaut001 <funkornaut@gmail.com>
2023-10-31 11:48:42 -0500

    dependencies reload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider, coinbaseWallet, metamaskWallet, paperWallet, smartWallet } from "@thirdweb-dev/react";
+import { ThirdwebProvider, localWallet, coinbaseWallet, metamaskWallet, paperWallet, smartWallet } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-
+import { NavBar } from "../components/hamburger";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
 const activeChain = "base-goerli";
+
+export const smartWalletConfig = smartWallet(localWallet(), {
+  factoryAddress: "0x7f7Dd55bB48E8f4C64e9304b3Da7A423Ef5Ab5ec",
+  gasless: true,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -13,19 +18,12 @@ function MyApp({ Component, pageProps }: AppProps) {
       clientId= {process.env.NEXT_PUBLIC_CLIENT_ID}
       activeChain={activeChain}
       supportedWallets={[
-        smartWallet({
-          factoryAddress: "0x7f7Dd55bB48E8f4C64e9304b3Da7A423Ef5Ab5ec",
-          gasless: true,
-          personalWallets: [
-            paperWallet({
-              paperClientId: "04c3e31c-8c02-4e67-9f1e-f4bd4ff12df0",
-            }),
-            metamaskWallet(),
-            coinbaseWallet(),
-          ]
-        })
+        smartWalletConfig
+          //   metamaskWallet(),
+          //   coinbaseWallet(),
       ]}
     >
+      <NavBar />
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
